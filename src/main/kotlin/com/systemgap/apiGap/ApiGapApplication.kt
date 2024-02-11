eed524bb67fd11f7be961ec686d5450df91b287e
MatheusBezerra414 <matheusrpbezerra@gmail.com>
2024-02-11 18:25:04 -0300

    initial project

---
+++ src/main/kotlin/com/systemgap/apiGap/ApiGapApplication.kt
@@ -0,0 +1,11 @@
+package com.systemgap.apiGap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGapApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGapApplication>(*args)
+}
---
+++ src/test/kotlin/com/systemgap/apiGap/ApiGapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.systemgap.apiGap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
